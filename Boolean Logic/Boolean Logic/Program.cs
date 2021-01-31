ad8da64dd10abd11b626ffbe9c3aced6fbe1bf7b
hansenu <ucabhansen@hotmail.com>
2021-01-31 17:25:41 -0500

    Loop Assignment
    Added while, do while, switch

--- Boolean Logic/Boolean Logic/Program.cs
+++ Boolean Logic/Boolean Logic/Program.cs
@@ -266,6 +266,50 @@ namespace Boolean_Logic
 
             //Console.WriteLine(result);
             //Console.ReadLine();
+
+            //SWITCH statements ----------------------------------------
+
+            //Will get the user's input and convert it to int
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //Add bool for while loop
+            bool isGuessed = number == 12;
+
+            //add do to have a feedback when isGuessed is true(12)
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            //will keep guessing correct number
+            while (!isGuessed);
+           
+            Console.ReadLine();
         }
     }
 }
